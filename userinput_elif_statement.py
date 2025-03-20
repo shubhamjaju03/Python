bb471e2160d55b5d7b7834a1298a2df36b238b16
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2025-03-20 14:08:35 +0530

    Add files via upload

---
+++ userinput_elif_statement.py
@@ -0,0 +1,7 @@
+light=input("Enter The light color : ")
+if(light=="green"):
+  print("Go")
+if(light=="yellow"):
+  print("Wait and start your engine")
+if(light=="Red"):
+  print("stop") #elif statement note the python is case4 sensitive so Red will have R capital here not small
