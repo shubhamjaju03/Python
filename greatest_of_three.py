9de5b62f6ed5c97f7479873fa94c3c39fc985060
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2025-03-20 14:51:11 +0530

    Add files via upload

---
+++ greatest_of_three.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the number 1 : "))
+b=int(input("Enter the number 2 : "))
+c=int(input("Enter the number 3 : "))
+if(a>b and a>c):
+  print("The number which is greater is ",a)
+elif(b>a and b>c):
+  print("The number which is greater is ",b)
+elif(c>a and c>b):
+  print("The number which is greater is ",c)
