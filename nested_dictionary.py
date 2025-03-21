e516b13d9ee6b5b7d7f597812958ab62de4cf9c5
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2025-03-21 14:40:20 +0530

    Add files via upload

---
+++ nested_dictionary.py
@@ -0,0 +1,8 @@
+student = {
+    "Name" : "Shubham Jaju",
+    "Subjects" : {                #another dictionary within this code
+        "phy" : "89",
+        "chemistry" :  "87"
+    }
+}
+print(student) #nested dictionary  
